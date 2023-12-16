1b36317129781cbff267b0c42aa2fb5ff90d7d3d
Muhammad Zano Afnan H <zanoafnan@students.unnes.ac.id>
2023-12-16 21:35:02 +0700

    testing

--- src/retrieve/Creds.jsx
+++ src/retrieve/Creds.jsx
@@ -1,9 +1,8 @@
 const Creds = () => {
-
     return {
-        ClientId: 'f190c4eb42424a6f9646f99583a44a82',
-        ClientSecret: '1a680c39d4594dd9966721d4b8cb2f17'
-    }
-}
+        ClientId: process.env.ClientId,
+        ClientSecret: process.env.ClientSecret
+    };
+};
 
 export { Creds };
